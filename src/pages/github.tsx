f39adc092dd9104f795a12e4e1b9e24319945dcc
Winicius-Neves <winiciusneves2004@hotmail.com>
2024-09-18 14:42:21 -0300

    delete .env

--- src/pages/github.tsx
+++ src/pages/github.tsx
@@ -15,7 +15,7 @@ interface Repository {
 }
 
 export default function Github() {
-    const GITHUB_TOKEN = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
+    const GITHUB_TOKEN = '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';
     const [repos, setRepos] = useState<Repository[]>([]);
     const [loadingLanguages, setLoadingLanguages] = useState(false);
 
